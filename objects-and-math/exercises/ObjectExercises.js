5b0d79f436e21e4e77636faf02a655008b089396
chris broadway <chrissilva215@aol.com>
2024-07-09 16:29:17 -0400

    still working

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -12,6 +12,36 @@ let salamander = {
    age: 5
 };
 
+let superChimpTwo = {
+   name: "Brad",
+   species: "Chimpanzee",
+   mass: 11,
+   age: 6,
+   astronautID: 3,
+   move: function () {return Math.floor(Math.random()*11)}
+};
+
+let dog = {
+   name: "Leroy",
+   species: "Beagle",
+   mass: 14,
+   age: 5,
+   astronautID: 4,
+   move: function () {return Math.floor(Math.random()*11)}
+};
+
+let waterBear = {
+   name: "Almina",
+   species: "Tardigrade",
+   mass: 0.0000000001,
+   age: 1,
+   astronautID: 5,
+};
+
+let crew = [superChimpOne, superChimpTwo, salamander, dog, waterBear];
+
+
+let crew = [superChimpOne, superChimpTwo, salamander, dog, waterBear];
 
 // After you have created the other object literals, add the astronautID property to each one.
 
